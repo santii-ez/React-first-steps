85108de16cd1ecc99e072fa7c2a5b881886af98b
PeterSauma <pedrosaumacammi@gmail.com>
2022-05-03 21:08:10 -0300

    se da estructura basica al entry point para levantar un servidor

--- app.js
+++ app.js
@@ -19,11 +19,20 @@ app.listen(port, () => {
 const views = path.join(__dirname, 'views/')
 // public
 const public = path.join(__dirname, 'public/')
+
 // hhtp routes
 const hhtpRaiz = '/'
-const htpptHome = '/home'
+const htppHome = '/home'
+const httpCarrito = '/Carrito'
+const httpLogIn = '/logIn'
+const httpProducto = '/producto'
+
 // html
 const homeHtml = 'home.html'
+const carritoHtml = 'carrito.html'
+const logInHtml ='logIn.html'
+const productoHtml = 'producto.html'
+
 
 // Define the static file path
 app.use(express.static(__dirname +'/public/'));
@@ -32,11 +41,18 @@ app.get(hhtpRaiz, (req, res) => {
     res.sendFile(path.join(views, homeHtml))
 })
 
-app.get(htpptHome, (req, res) => {
+app.get(htppHome, (req, res) => {
     res.sendFile(path.join(views, homeHtml))
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-    console.log(__dirname)
+app.get(httpCarrito, (req, res) => {
+    res.sendFile(path.join(views, carritoHtml))
+})
+
+app.get(httpLogIn, (req, res) => {
+    res.sendFile(path.join(views, logInHtml))
+})
+
+app.get(httpProducto, (req, res) => {
+    res.sendFile(path.join(views, productoHtml))
 })
