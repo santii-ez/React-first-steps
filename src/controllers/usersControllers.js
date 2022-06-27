26fa411b3104a043595d63bede0f23d072ab7334
jorabdo <mjabdo@hotmail.com>
2022-06-27 13:17:23 +0800

    registro

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,9 +1,32 @@
 const fs = require ('fs');
 const path = require ('path');
+const { validationResult }= require('express-validator');
+
 
 const controller = {
-    logIn: (req, res) => {
+    login: (req, res) => {
         res.render('login')},
+
+    processRegister: (req,res)=>{
+
+        const resultValidation = validationResult(req);
+        
+        
+
+        if(resultValidation.errors.length > 0){
+
+            return res.render('login', {
+
+                errors : resultValidation.mapped(),
+                oldData : req.body
+
+            })
+            
+        }
+
+        res.redirect('/')
+
+    },
 };
 
 module.exports = controller;
--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -4,8 +4,36 @@ const usersControllers = require('../controllers/usersControllers');
 const path = require("path");
 const multer = require("multer");
 
+//multer
 
-router.get("/login", usersControllers.logIn);
+const storage = multer.diskStorage({
+    destination:(req, file, cb)=>{
+        cb (null,'../public/images/users');
+    },
+
+    filename: (req, file, cb)=>{
+        let fileName = `${Date.now()}_img${path.extname(file.originalname)}`
+        cb (null, fileName)
+    }
+})
+
+const upLoadFile = multer({storage})
+
+//requerir express validator
+
+const { body } = require('express-validator')
+const validations = [
+    body('fullName').notEmpty().withMessage('Tienes que ingresar su nombre'),
+    body('lastName').notEmpty().withMessage('Tienes que ingresar su apellido'),
+    body('email').notEmpty().withMessage('Tienes que ingresar su email'),
+    body('contrasena').notEmpty().withMessage('Tienes que ingresar su contraseña'),
+]
+
+// Formulario de registro y login
+router.get("/login", usersControllers.login);
+
+// Procesar el registro
+router.post("/login", upLoadFile.single('avatar'), validations, usersControllers.processRegister)
 
 
 module.exports = router;
