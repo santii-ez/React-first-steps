a6315bf7d6881d998ec4f0af894be89d7e48e23f
jorabdo <mjabdo@hotmail.com>
2022-08-04 10:33:24 +0800

    burgerMenu desplegable

---
+++ public/js/burgerMenu.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", function () {
+
+    const iconoMenu = document.querySelector('.burger-menu');
+    
+    const menu = document.querySelector('#menu');
+    const burgerClose = document.querySelector('.burger-close')
+    const href = document.querySelectorAll('.href a')
+    const card = document.querySelectorAll('article')
+    
+    iconoMenu.addEventListener('click', function() {
+        iconoMenu.style.display = "none"
+        menu.classList.toggle('active');
+        document.body.classList.toggle('opacity')
+    })
+    
+    burgerClose.addEventListener('click', function(){
+        iconoMenu.style.display = "block"
+        menu.classList.toggle('active')
+    })
+
+  card.addEventListener('mouseover', function(){
+    card.classList.toggle('opacity')
+  })
+})
