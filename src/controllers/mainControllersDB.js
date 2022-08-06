1ac1769afa041cdfd3f099e5718914c34f7f745d
PeterSauma <pedrosaumacammi@gmail.com>
2022-08-06 16:08:30 -0300

    se arregla el mainControllerDB, se agregan las funciones search, y listados por section y se agregan las rutas de acceso

--- src/controllers/mainControllersDB.js
+++ src/controllers/mainControllersDB.js
@@ -1,5 +1,6 @@
 // Requires
 const db = require ('../database/models');
+const Sequelize = require ('sequelize')
 const Op = db.Sequelize.Op;
 const fs = require ('fs');
 const path = require ('path');
@@ -8,23 +9,57 @@ const { where } = require('sequelize/types');
 // controller DB
 
 const controller = {
-    prueba : (req, res)=>{
-        db.Product.findAll()
-        .then ((product)=>{
+    //index.ejs
+    prueba : async(req, res)=>{
             let article = ["article01", "article02", "article03", "article04"];
-            let searchedProducts = product.section == 'lo mas buscado'
-            let phones = product.category == 'phones'
-            let tablets = product.category == 'tablets'
-            let laptops = product.category == 'laptops'
-            return   res.render(path.join(__dirname,'../views/prueba'), {article: article,
-                                                                        searchedProducts: searchedProducts, 
-                                                                        phones: phones, 
-                                                                        tablets :tablets, 
-                                                                        laptops: laptops});
-        })
+            let searchedProducts = await db.Product.findAll({ where : {section:'lo mas buscado'}});
+            let phones = await db.Product.findAll({ where : {section:'celulares'}});
+            let tablets = await db.Product.findAll({ where : {section:'tablets'}}); 
+            let laptops = await db.Product.findAll({ where : {section:'laptops'}}); 
+            return   res.render(path.join(__dirname,'../views/prueba'), {article: article, searchedProducts: searchedProducts, phones: phones, tablets :tablets, laptops: laptops})
+    },
+    //lista de lo más buscado
+    searchedProducts: (req,res) =>{
+        db.Product.findAll({where:{section:'lo mas buscado'}})
+            .then((products) => {
+                return res.render(path.join(__dirname,'../views/listProducts'), { products: products });
+            })
+        .catch(error => res.send(error));
+    },
+    //lista de phones
+    phones: (req,res) =>{
+        db.Product.findAll({where:{section:'phones'}})
+            .then((products) => {
+                return res.render(path.join(__dirname,'../views/listProducts'), { products: products });
+            })
+        .catch(error => res.send(error));
+    },
+    //lista de tablets
+    tablets: (req,res) =>{
+        db.Product.findAll({where:{section:'tablets'}})
+            .then((products) => {
+                return res.render(path.join(__dirname,'../views/listProducts'), { products: products });
+            })
+        .catch(error => res.send(error));
+    },
+    //lista de laptops
+    laptops: (req,res) =>{
+        db.Product.findAll({where:{section:'laptops'}})
+            .then((products) => {
+                return res.render(path.join(__dirname,'../views/listProducts'), { products: products });
+            })
+        .catch(error => res.send(error));
+    },
+    //formulario de busqueda
+    search: (req,res) =>{
+        //capturo el string que busca el usuaria que viaja por la url
+        const {search}= req.params.search
+        db.Product.findAll({where:{name: {[Op.like]:'%'+search+'%'}}})
+            .then((products) => {
+                return res.render(path.join(__dirname,'../views/listProducts'), { products: products });
+            })
         .catch(error => res.send(error));
     }
-    
 }
 
 
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -9,6 +9,11 @@ router.get("/home", mainControllersJSON.index);
 
 //routes DB
 router.get("/db", mainControllersDB.prueba);//por ahora levanta un ejs de prueba
+router.get('/searchedProducts',mainControllersDB.searchedProducts)//ruta de acceso a lista de searchedProducts
+router.get('/phones',mainControllersDB.phones)//ruta de acceso a lista de phones
+router.get('/tablets',mainControllersDB.tablets)//ruta de acceso a lista de tablets
+router.get('/laptops',mainControllersDB.laptops)//ruta de acceso a lista de laptops
+router.get('/search',mainControllersDB.search)//ruta de acceso a la barra de busqueda
 
 // exports
 module.exports = router;
